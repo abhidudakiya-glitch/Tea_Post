6a858d0f441b233550b8854c4fa5f19a4ba7a4f0
abhidudakiya-glitch <abhidudakiyaskau@gmail.com>
2026-02-01 19:54:34 +0530

    Update model file paths to Korean Pizza

--- Korean Pizza/script.js
+++ Korean Pizza/script.js
@@ -1,7 +1,7 @@
 (function () {
 
-    const glbFile = "Models/Burger.glb";
-    const usdzFile = "Models/Burger.usdz";
+    const glbFile = "Models/Korean Pizza.glb";
+    const usdzFile = "Models/Korean Pizza.usdz";
 
     const ua = navigator.userAgent || navigator.vendor || window.opera;
 
@@ -16,7 +16,7 @@
     // ---------- Android ----------
     if (/android/i.test(ua)) {
 
-        const fullGlbUrl = location.origin + "/Tea_Post/Burger/" + glbFile;
+        const fullGlbUrl = location.origin + "/Tea_Post/Korean Pizza/" + glbFile;
 
         const sceneViewerIntent =
             "intent://arvr.google.com/scene-viewer/1.0" +
